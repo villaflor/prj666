2aa768c278ffdd39e70c6b64ccc71a546f68489a
brian <brian@pying.ca>
2017-07-09 13:17:31 -0400

    Red search

--- includes/templates/red/header.php
+++ includes/templates/red/header.php
@@ -3,8 +3,8 @@
     <!-- search bar -->
     <div class="row">
       <div class="col-md-4 col-md-offset-9 col-sm-6 col-sm-offset-6 col-xs-offset-3 col-xs-8">
-        <form action="/action_page.php">
-          <input type="text" name="good" value="">
+        <form action="" method="GET">
+          <input type="text" name="keyword" value="">
           <input type="submit" value="search">
         </form>
       </div>
--- tools/searchTest.php
+++ tools/searchTest.php
@@ -9,16 +9,16 @@
 
     // call get all
 	echo '<br/>Get all<br/>';
-    $alldata = $search->getAll();
+    $alldata = $search->getAll(10,0);
     while ($row = mysqli_fetch_assoc($alldata)) {
         print_r($row);
         echo "<br/>";
     }
 
     //search
-    $keyword = "checkbox";
+    $keyword = "Battle";
     echo "<br/>search with keyword $keyword<br/>";
-    $alldata = $search->searchGood($keyword);
+    $alldata = $search->searchGood($keyword,10,0);
     while ($row = mysqli_fetch_assoc($alldata)) {
         print_r($row);
         echo "<br/>";
