96c0b41d03263b86c0e40a72226c9166adee3a4c
Olga Arsenieva <oarsenieva@myseneca.ca>
2017-07-12 14:50:01 -0400

    delete-good + create-good update part 2
    delete-good and create-good scripts 
    this is part 2 of update, with scripts for tools folder that are needed by files in blue folder

--- tools/good.php
+++ tools/good.php
@@ -30,6 +30,27 @@ class Good {
         return $this->mysqli->query($sql_query);
     }
 
+   /*
+    Get all goods with limit function
+    return good list
+    */
+    public function getGoodRows($categoryid, $limit=0, $offset=0){
+        if($categoryid === "*"){
+             $sql_query = "SELECT g.good_id as good_id, g.good_name as good_name, g.good_image as good_image, g.good_description as good_description, g.good_price as good_price, 
+                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, c.client_id as client_id
+                        from good g join category c on g.category_id = c.category_id
+                        limit $limit offset $offset"; //where c.client_id=1 
+        }else{
+            $sql_query = "SELECT g.good_id as good_id, g.good_name as good_name, g.good_image as good_image, g.good_description as good_description, g.good_price as good_price, 
+                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, c.client_id as client_id
+                        from good g join category c on g.category_id = c.category_id
+                        where c.category_id = $categoryid
+                        limit $limit offset $offset"; //where c.client_id=1 
+        }
+
+        return $this->mysqli->query($sql_query);
+    }
+
     /*
     Get one good
     return good list
@@ -52,15 +73,15 @@ class Good {
     public function addGood($name, $image, $description, $price, $stock, $weight, $taxable, $visible, $category){
        
        $sql_query = "INSERT INTO `good`(`good_name`, `good_image`, `good_description`, `good_price`, 
-                      `good_in_stock`, `good_weight`, `good_taxable`, `good_visible`, `category_id`) 
+                      `good_in_stock`, `good_weight`, `good_taxable`, `good_visible`, `category_id`, `sale_id`) 
                     VALUES ('$name', '$image', '$description', $price,
-                        $stock, $weight, $taxable, $visible, $category)";
+                        $stock, $weight, $taxable, $visible, $category, 16)";
 
         if($this->mysqli->query($sql_query)){
-            echo "good.php:query worked <br/>";
+            echo "production/tools/good.php:query worked ".$sql_query."<br/>";
             return true;
         }else{
-            echo "good.php:received error ";//.mysqli_error($this->mysqli->query($sql_query))."<br/>";
+            echo "production/tools/good.php:received error for query ".$sql_query." <br/>";
             return false;
         }
     }
@@ -85,7 +106,7 @@ class Good {
     */
 
     public function deleteGood($id){
-       $sql_query = "DELETE FROM `good` WHERE `good_id`=65";
+       $sql_query = "DELETE FROM `good` WHERE `good_id`=$id";
         return $this->mysqli->query($sql_query);
     }
 
