32e460c76838759c8e8a474bdd8606ee907e2ee5
Peiying Yang <pyang16@myseneca.ca>
2017-07-19 17:50:47 -0400

    blue add about us footer

--- includes/templates/blue/Footer.php
+++ includes/templates/blue/Footer.php
@@ -1,3 +1,9 @@
 <footer>
-			
+     <?php
+     $url = "/data/www/default/wecreu/companyInfo/footer/".$clientId.".txt";
+     if (file_exists($url)) {
+       $content = file_get_contents($url);
+       echo $content;
+     }
+     ?>
 </footer>
