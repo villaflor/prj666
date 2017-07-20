7d8a457b6e49e2e0dcf598c8f8729ed1c14ae067
brian <brian@pying.ca>
2017-07-19 20:20:41 -0400

    email

--- tools/email.php
+++ tools/email.php
@@ -1,41 +1,36 @@
 <?php
     if($_POST && $_POST["method"] == "sendEmail"){
         require_once('email/PHPMailerAutoload.php');
-        //require_once("../../../tools/email/");
         $mail = new PHPMailer;
-    
+
         //$mail->SMTPDebug = 3;                               // Enable verbose debug output
         function safe_replace($string, $isNBSP=true) {
-            $string = trim($string); 
-            $string = str_replace('%20','',$string);
-            $string = str_replace('%27','',$string);
-            $string = str_replace('%2527','',$string);
-            $string = str_replace('*','',$string);
-            $string = str_replace('"','',$string);
-            $string = str_replace("'",'',$string);
-            $string = str_replace('"','',$string);
-            $string = str_replace(';','',$string);
-            $string = str_replace('<','',$string);
-            $string = str_replace('>','',$string);
-            $string = str_replace("{",'',$string);
-            $string = str_replace('}','',$string);
-			$string = nl2br($string, false);
-            
+            $string = trim($string);
             if($isNBSP){
-            	$string = str_replace(' ','&nbsp;',$string);
+                $string = str_replace(' ','&nbsp;',$string);
+                $string = nl2br($string);
             }
-            
             return $string;
         }
-		
+
         // Check strings
-		$name = safe_replace($_POST["name"]);
-		$from = safe_replace($_POST['email']);
-		$subject = safe_replace($_POST['your-subject'],false);
-		$message = safe_replace($_POST['message']);
+    	$name = safe_replace($_POST["name"]);
+    	$from = safe_replace($_POST['email']);
+    	$subject = safe_replace($_POST['your-subject'],false);
+    	$message = safe_replace($_POST['message']);
+    	date_default_timezone_set("Canada/Central");
+    	$time = date('Y-m-d h:i:sa');
+
+
+        // Check strings
+        $to = $_POST["to"];
+		$name = $_POST["name"];
+		$from = $_POST['email'];
+		$subject = $_POST['your-subject'];
+		$message = $_POST['message'];
 		date_default_timezone_set("Canada/Central");
 		$time = date('Y-m-d h:i:sa');
-	
+
         // Config smtp server
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = ' smtp.zoho.com';  // Specify main and backup SMTP servers
@@ -44,31 +39,30 @@
         $mail->Password = 'vLrJatJGqEuL';                           // SMTP password
         $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 465;                                    // TCP port to connect to
-        
+
         $mail->setFrom('wecreu@pying.ca', 'Message From wecreu');
-        //TODO
-        $mail->addAddress('py@pying.ca', '');     // Add a recipient
-        $mail->addCC($from);
-        //$mail->addReplyTo('py@pying.ca', 'Peiying (Brian)');
-        $mail->isHTML(true);                                  // Set email format to HTML
-        
+        $mail->addAddress($to, '');     // Add a recipient
+        // $mail->addCC($from);
+        // $mail->addReplyTo('py@pying.ca', 'Peiying (Brian)');
+        $mail->isHTML(false);                                  // Set email format to HTML
+
         $mail->Subject = "$subject";
         $mail->Body    = "Time: $time<br/>Name: $name<br/>From: $from<br/> Subject: $subject<br/>Message:<br/>$message";
         $mail->AltBody = "Time: $time<br/>Name: $name<br/>From: $from<br/> Subject: $subject<br/>Message:<br/>$message";
-        
+
         // Send email and Check if the email has been sent
         if($mail->send()) {
         ?>
             <script>
                 alert("Thank you. Your message has been sent.");
-                window.location.href="<?php echo $_POST['url'];?>";  
+                window.location.href="<?php echo $_POST['url'];?>";
             </script>
         <?php
         } else {
         ?>
             <script>
                 alert("There is error, you message cannot be sent.");
-                window.location.href="<?php echo $_POST['url'];?>";  
+                window.location.href="<?php echo $_POST['url'];?>";
             </script>
         <?php
         }
