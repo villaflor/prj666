f492293464b010a1b617b3e48d0a88d0b2ea13b6
Olga Arsenieva <oarsenieva@myseneca.ca>
2017-07-24 11:54:14 -0400

    tools update again

--- tools/good.php
+++ tools/good.php
@@ -20,14 +20,34 @@ class Good {
 
         if($categoryid === "*"){
              $sql_query = "SELECT g.good_id as good_id, g.good_name as good_name, g.good_image as good_image, g.good_description as good_description, g.good_price as good_price, 
-                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, c.client_id as client_id
+                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, g.good_in_stock as good_in_stock, c.client_id as client_id
                         FROM good g JOIN category c ON g.category_id = c.category_id WHERE c.client_id = $clientid";
         }else{
             $sql_query = "SELECT g.good_id as good_id, g.good_name as good_name, g.good_image as good_image, g.good_description as good_description, g.good_price as good_price, 
-                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, c.client_id as client_id
+                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, g.good_in_stock as good_in_stock, c.client_id as client_id
                         FROM good g JOIN category c ON g.category_id = c.category_id
                         WHERE c.category_id = $categoryid AND c.client_id = $clientid";
         }
+     //   echo "clientId is ".$clientid." SQL QUERY ".$sql_query;
+        return $this->mysqli->query($sql_query);
+    }
+
+    /*
+    Get all goods for wecreu site that receives a client id. all goods per client only
+    return good list
+    */
+    public function getMgmtGoods($client){
+
+     //   if($categoryid === "*"){
+             $sql_query = "SELECT g.good_id as good_id, g.good_name as good_name, g.good_image as good_image, g.good_description as good_description, g.good_price as good_price, 
+                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, g.good_in_stock as good_in_stock, c.client_id as client_id
+                        FROM good g JOIN category c ON g.category_id = c.category_id WHERE c.client_id = $client";
+        /*}else{ left in case need category later
+            $sql_query = "SELECT g.good_id as good_id, g.good_name as good_name, g.good_image as good_image, g.good_description as good_description, g.good_price as good_price, 
+                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, g.good_in_stock as good_in_stock, c.client_id as client_id
+                        FROM good g JOIN category c ON g.category_id = c.category_id
+                        WHERE c.category_id = $categoryid AND c.client_id = $clientid";
+        }*/
      //   echo "clientId is ".$clientid." SQL QUERY ".$sql_query;
         return $this->mysqli->query($sql_query);
     }
@@ -40,12 +60,12 @@ class Good {
         $clientid = file_get_contents('/data/www/default/wecreu/includes/templates/blue/conf.ini');
         if($categoryid === "*"){
              $sql_query = "SELECT g.good_id as good_id, g.good_name as good_name, g.good_image as good_image, g.good_description as good_description, g.good_price as good_price, 
-                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, c.client_id as client_id
+                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, g.good_in_stock as good_in_stock, c.client_id as client_id
                         FROM good g JOIN category c ON g.category_id = c.category_id WHERE c.client_id = $clientid
                         LIMIT $limit OFFSET $offset"; // 
         }else{
             $sql_query = "SELECT g.good_id as good_id, g.good_name as good_name, g.good_image as good_image, g.good_description as good_description, g.good_price as good_price, 
-                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, c.client_id as client_id
+                        c.category_id as category_id, c.category_name as category_name, c.category_description as category_description, g.good_in_stock as good_in_stock, c.client_id as client_id
                         FROM good g JOIN category c ON g.category_id = c.category_id
                         where c.category_id = $categoryid WHERE c.client_id = $clientid
                         LIMIT $limit OFFSET $offset"; 
@@ -67,6 +87,7 @@ class Good {
                             s.sale_id as sale_id, s.sale_name as sale_name
                     FROM good g join category c on g.category_id = c.category_id
                                 join sale s on g.sale_id = s.sale_id where g.good_id = $id";
+        echo "<br/>QUERY: ".$sql_query."<br/>";
         return $this->mysqli->query($sql_query);
     }
 
