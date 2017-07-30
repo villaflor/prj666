31650a58cd321c09a0f717c46169067b579e972b
Olga Arsenieva <oarsenieva@myseneca.ca>
2017-07-30 13:31:56 -0400

    HeaderFix

--- includes/templates/blue/Header.php
+++ includes/templates/blue/Header.php
@@ -3,17 +3,20 @@
         include '/data/www/default/wecreu/tools/good.php';
         include '/data/www/default/wecreu/tools/category.php';
         include_once '/data/www/default/wecreu/tools/sql.php';
+        include_once('/data/www/default/wecreu/tools/client.php');
+
         $clientid = file_get_contents('conf.ini');
 
         $db = Database::getInstance();
         $category = new Category($db,$clientid);
         $allcategory = $category->getAll();
+        $client = new Client($db,$clientid);
     ?>
-	<img src="../../images/logo.jpg" alt="logo" height="130" width="140" />
+	<img src="images/logo.jpg" alt="logo" height="130" width="140" />
 			
 	<form>
 		<input type="text" name="keyword" /> Search
 	</form>
-	<h1>Slideshow Template</h1>
+	<h1><?php echo $client->getClientSiteTitle(); ?></h1>
 			
 </header>
