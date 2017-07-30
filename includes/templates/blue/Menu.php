07d634e8394f6b9dba517bbdb7e6ad6d1fe55b2f
Olga Arsenieva <oarsenieva@myseneca.ca>
2017-07-30 18:10:32 -0400

    Add files via upload

--- includes/templates/blue/Menu.php
+++ includes/templates/blue/Menu.php
@@ -1,8 +1,8 @@
 <nav>
 	<ul>
 		<li><a href="index.php">Home</a></li>
-		<li><a href="index.php">Help</a></li>
-		<li><a href="cart.php">Shopping Cart</a></li>
+		<!--<li><a href="index.php">Help</a></li>-->
+		<li><a href="viewCart.php">Shopping Cart</a></li>
 		<li><a href="About.php">About</a></li>
 	</ul>
 </nav>
