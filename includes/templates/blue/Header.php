c5c1cf729585c31e216ad148e86f203d9711c31e
Olga Arsenieva <oarsenieva@myseneca.ca>
2017-07-30 13:36:28 -0400

    Header/Footer fix

--- includes/templates/blue/Header.php
+++ includes/templates/blue/Header.php
@@ -4,13 +4,14 @@
         include '/data/www/default/wecreu/tools/category.php';
         include_once '/data/www/default/wecreu/tools/sql.php';
         include_once('/data/www/default/wecreu/tools/client.php');
-
+        include_once("/data/www/default/wecreu/tools/page.php");
         $clientid = file_get_contents('conf.ini');
 
         $db = Database::getInstance();
         $category = new Category($db,$clientid);
         $allcategory = $category->getAll();
         $client = new Client($db,$clientid);
+        $page = new Page($db,$clientid);
     ?>
 	<img src="images/logo.jpg" alt="logo" height="130" width="140" />
 			
