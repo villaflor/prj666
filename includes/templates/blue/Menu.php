024b73f5d6d44430f80738e7de0a894df0daf517
Quang Nguyen <quangpersie1@gmail.com>
2017-08-01 16:55:12 -0400

    Update Menu.php

--- includes/templates/blue/Menu.php
+++ includes/templates/blue/Menu.php
@@ -4,5 +4,8 @@
 		<!--<li><a href="index.php">Help</a></li>-->
 		<li><a href="viewCart.php">Shopping Cart</a></li>
 		<li><a href="About.php">About</a></li>
+		<?php if ($contact == 1 ){ ?>		
+ -		<li><a href="contact-us.php">Contact us</a></li>		
+ -		<?php }?>
 	</ul>
 </nav>
