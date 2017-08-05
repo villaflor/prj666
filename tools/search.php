0d97323857f84c94341ca0c974eb0227be717c46
Peiying Yang(Brian) <mryang91@hotmail.com>
2017-08-05 11:53:34 -0400

    Update search.php

--- tools/search.php
+++ tools/search.php
@@ -42,7 +42,7 @@ class Search {
 		$keyword = explode("]", $keyword)[0];
 		$keyword = explode("}", $keyword)[0];
 		$keyword = explode("{", $keyword)[0];
-    $keyword = explode('\\', $keyword)[0];
+                $keyword = explode('\\', $keyword)[0];
 		$keyword = explode("|", $keyword)[0];
 		$keyword = explode("<", $keyword)[0];
 		$keyword = explode(">", $keyword)[0];
@@ -50,13 +50,13 @@ class Search {
 		$keyword = trim($keyword);
 		$sql_query = "SELECT * FROM v_category_good WHERE category_display = 1 AND client_id = $this->client_id AND good_visible = 1 AND (good_name LIKE '%$keyword%' OR category_name LIKE '%$keyword%' OR good_description LIKE '%$keyword%') ORDER BY good_id DESC LIMIT $limit OFFSET $offSet";
 
-	  return $this->mysqli->query($sql_query);
+	        return $this->mysqli->query($sql_query);
 	}
 
 	// Constructor
 	public function __construct($db, $client_id) {
 		$this->mysqli = $db->getConnection();
-    $this->client_id = $client_id;
+                $this->client_id = $client_id;
 	}
 
 	// Magic method clone is empty to prevent duplication of connection
