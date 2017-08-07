87230b04146b10ee7d219b0ef56ad7acb7d374e8
Olga Arsenieva <oarsenieva@myseneca.ca>
2017-08-06 22:05:11 -0400

    Add files via upload

--- includes/templates/green/index.php
+++ includes/templates/green/index.php
@@ -1,13 +1,24 @@
 <?php
-    /*$clientId = file_get_contents('conf.ini');
-    include_once('/data/www/default/wecreu/tools/category.php');
-    include_once("/data/www/default/wecreu/tools/sql.php");
+    /*include_once('/data/www/default/wecreu/tools/category.php');
     include_once("/data/www/default/wecreu/tools/client.php");
-
 	//create an object
-    $db = Database::getInstance();
     $category = new Category($db,$clientId);
     $client = new Client($db,$clientId);*/
+
+    include_once("/data/www/default/wecreu/tools/sql.php");
+    $clientId = file_get_contents('conf.ini');
+    $db = Database::getInstance();
+
+    $query="SELECT sale.* FROM client JOIN category ON category.client_id = client.client_id JOIN good ON good.category_id = category.category_id JOIN sale ON sale.sale_id = good.sale_id WHERE client.client_id = ".$clientId." GROUP BY sale.sale_id LIMIT 4";
+  //  echo $query;
+    $conn = $db->getConnection();  
+    $allsale = $conn->query($query);
+    $rowcount = mysqli_num_rows($allsale);
+   // echo "rowcount ".$rowcount;
+
+    $saleHeading = array(" ", " ", " ", " ");
+   // print_r($saleHeading);
+
     ?>
 
 <!DOCTYPE html>
@@ -40,33 +51,59 @@
 </div>
 <div class="container mb-5">
     <div class="row">
+    <?php
+        
+        if($rowcount == 0){
+           
+    ?>
+           <h2 class="text-center mb-5"> 
+                Welcome! Enjoy your shopping!
+           </h2>
+ 
+    <?php
+        } else if($rowcount >= 1){
+    ?>
         <section class="col-md-6">
-            <div class="row">
-                <section class="col-6 mb-5">
-                    <img class="img-thumbnail" src="images/cow.jpg" alt="cow">
+    <?php
+            for($i = 0; $i < $rowcount; $i++ ){
+                $salerow = mysqli_fetch_assoc($allsale);
+                $goodquery="SELECT good_id, good_image FROM good WHERE sale_id = ".$salerow['sale_id']." LIMIT 1";
+                $goodinfo = $conn->query($goodquery);
+                $goodrow = mysqli_fetch_assoc($goodinfo);
+
+                $saleHeading[$i] = $salerow['sale_name']."! "./*substr(*/$salerow['discount']/*, 0, -3)*/."% OFF ".$salerow['sale_description'];
+
+                if($i%2 == 0){
+                    echo '<div class="row">';
+                }
+        ?>
+                <section class="col-6 mb-5"  >
+                    <a href="detail.php?gid=<?php echo $goodrow['good_id']; ?>" >
+                        <img class="img-thumbnail" src=<?php echo "/wecreu/images/".$goodrow['good_image']; ?> alt="cow" />
+                    </a>
                 </section>
-                <section class="col-6">
-                    <img class="img-thumbnail" src="images/cow.jpg" alt="Wecreu Logo">
-                </section>
-            </div>
-            <div class="row">
-                <section class="col-6">
-                    <img class="img-thumbnail" src="images/cow.jpg" alt="Wecreu Logo">
-                </section>
-                <section class="col-6">
-                    <img class="img-thumbnail" src="images/cow.jpg" alt="Wecreu Logo">
-                </section>
-            </div>
+        <?php
+                if($i%2 != 0){
+                    echo "</div>";
+                }
+            }
+    ?>
         </section>
 
-        <section class="col-md-6 pt-5">
-            <h2 class="text-center mb-5">
-                Cheesy Promo! 50% off by tomorrow! Get your Cows now!
-            </h2>
-            <h2 class="text-center">
-                New Cows in stock! Get it while they last! Also, Chickens coming for Canada Day!
-            </h2>
+       <section class="col-md-6 pt-5" >
+    <?php
+        foreach($saleHeading as $s){
+    ?>
+             <h2 class="text-center mb-5"> 
+                <?php echo $s; ?>
+           </h2>
+    <?php
+        }
+    ?>
         </section>
+    <?php            
+    }
+    ?>
     </div>
 </div>
 
